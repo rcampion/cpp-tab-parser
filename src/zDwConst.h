01115eca3dba40d7d620b5424c0463263220606e
Richard Campion <campion.richard@gmail.com>
2020-07-18 19:48:42 -0400

    Cleanup

--- src/zDwConst.h
+++ src/zDwConst.h
@@ -1,6 +1,6 @@
 /**************************************************************************
  *
- * zlexconst.h
+ * zDwConst.h
  * author: Richard Campion
  *  
  **************************************************************************/
@@ -21,9 +21,9 @@
 	#define PATHMAX	_MAX_PATH
   #define PATH_SEPARATOR ";"
 #else
-	#define SEPARATOR	"/"
-	#define PATHMAX 1024
-	#define PATH_SEPARATOR ":"
+#define SEPARATOR	"/"
+#define PATHMAX 1024
+#define PATH_SEPARATOR ":"
 #endif
 
 /***************************************************************************
@@ -119,23 +119,21 @@ typedef enum LEXTOKEN_DATATYPE {
  **************************************************************************/
 typedef enum LEXSCAN_STATUS {
 	LEXSCAN_UNKNOWN = 0, // no scan / no parse	/ no value								
-	LEXSCAN_ASSIGNED,    // scanned / parsed / value assigned - no error                 
+	LEXSCAN_ASSIGNED, // scanned / parsed / value assigned - no error
 	LEXSCAN_SEEN,        // scanned/parsed - no value  
-	LEXSCAN_ERROR,       // severe error                                               
+	LEXSCAN_ERROR, // severe error
 	LEXSCAN_PENDING,     // token scanned                              
-	LEXSCAN_WARNING,     // non-severe error																						
-	LEXSCAN_DELETED,     // remove from memory                                        
-	LEXSCAN_COMPLETE     // token values are set														
+	LEXSCAN_WARNING, // non-severe error
+	LEXSCAN_DELETED, // remove from memory
+	LEXSCAN_COMPLETE // token values are set
 } LexScanStatus;
-
 
 /***************************************************************************
  *
  * StatusCode enumeration
  *
  **************************************************************************/
-enum StatusCode
-{
+enum StatusCode {
 	E_NoError = 0,
 	E_BadParameter,
 	E_BufferEmpty,
@@ -155,18 +153,16 @@ enum StatusCode
 	E_OSSpecificError,
 	E_LexerError,
 	E_ParserError,
-  E_ValueNotFound
+	E_ValueNotFound
 };
 
-
-enum IteratorCode
-{
+enum IteratorCode {
 	NoError = 0,
 	NoMoreEntries,
 	ListUninitialized,
 	ListInitialized,
 	BadParameter,
-	OSSpecificError,					// call GetLastError() for Win32, or use "errno" variable for other platforms
+	OSSpecificError,// call GetLastError() for Win32, or use "errno" variable for other platforms
 	BufferOverflow,
 	NoStartDirectory,
 	DirectorySearchError,
@@ -181,31 +177,31 @@ enum IteratorCode
 #define RET_ARG_ERR									RET_PROG_BASE + 2			// problem with arguments
 #define RET_INT_ERR									RET_PROG_BASE + 3			// internal error in this application
 #define RET_FILE_ERR								RET_PROG_BASE + 4			// file handling error
-#define RET_BUF_OVERFLOW						RET_PROG_BASE + 5			// buffer overflow
-#define RET_UNINIT_ERR							RET_PROG_BASE + 6 		// unitialized data error
+#define RET_BUF_OVERFLOW							RET_PROG_BASE + 5			// buffer overflow
+#define RET_UNINIT_ERR								RET_PROG_BASE + 6 		// unitialized data error
 #define RET_OS_ERR									RET_PROG_BASE + 7			// OS specific error
-#define RET_NO_DIR_ERR							RET_PROG_BASE + 8 		// no directory available
-#define RET_DIR_SEARCH_ERR					RET_PROG_BASE + 9 		// cannot search a directory
-#define RET_FILE_OVERRUN						RET_PROG_BASE + 10		// went too far in file processing
-#define RET_EMPTY_LIST							RET_PROG_BASE + 11		// empty list
-#define RET_NOMORE_ENTRIES					RET_PROG_BASE + 12		// no more entries in a list
-#define RET_PRINT_ERR  					    RET_PROG_BASE + 13		// print routine error
-#define RET_EXCEPTION_ERR						RET_PROG_BASE + 14		// exception error in this application
-#define RET_PROG_BASE_MAX						RET_PROG_BASE + 29
+#define RET_NO_DIR_ERR								RET_PROG_BASE + 8 		// no directory available
+#define RET_DIR_SEARCH_ERR							RET_PROG_BASE + 9 		// cannot search a directory
+#define RET_FILE_OVERRUN							RET_PROG_BASE + 10		// went too far in file processing
+#define RET_EMPTY_LIST								RET_PROG_BASE + 11		// empty list
+#define RET_NOMORE_ENTRIES							RET_PROG_BASE + 12		// no more entries in a list
+#define RET_PRINT_ERR  					    		RET_PROG_BASE + 13		// print routine error
+#define RET_EXCEPTION_ERR							RET_PROG_BASE + 14		// exception error in this application
+#define RET_PROG_BASE_MAX							RET_PROG_BASE + 29
 
 // -> parser errors / status
-#define RET_PARSER_BASE							RET_PROG_BASE + 500
-#define RET_PARSER_INT_ERR					RET_PARSER_BASE + -1	// internal error in the parser
+#define RET_PARSER_BASE								RET_PROG_BASE + 500
+#define RET_PARSER_INT_ERR							RET_PARSER_BASE + -1	// internal error in the parser
 #define RET_PARSE_ERR								RET_PARSER_BASE + 0		// file could not be parsed
-#define RET_PARSE_HEADER_OK				  RET_PARSER_BASE + 1		// header line was parsed
-#define RET_PARSER_BASE_MAX					RET_PARSER_BASE + 200	// errors in between are parser specific
+#define RET_PARSE_HEADER_OK				  			RET_PARSER_BASE + 1		// header line was parsed
+#define RET_PARSER_BASE_MAX							RET_PARSER_BASE + 200	// errors in between are parser specific
 
 // parse status levels
-#define PARSE_LEVEL_NOT_PARSED			0
+#define PARSE_LEVEL_NOT_PARSED				0
 #define PARSE_LEVEL_PARSE_ERR				1
 #define PARSE_LEVEL_PARSE_OK				2
 
-#define LEXANALYSIS_BASE						    RET_PROG_BASE + 90
+#define LEXANALYSIS_BASE				RET_PROG_BASE + 90
 #define LEXANALYSIS_PARSE_INT_ERR       LEXANALYSIS_BASE +1
 #define LEXANALYSIS_PARSE_ERR           LEXANALYSIS_BASE +2
 #define LEXANALYSIS_PARAM_ERR           LEXANALYSIS_BASE +3
@@ -217,7 +213,7 @@ enum IteratorCode
 #define LEXANALYSIS_DEPOT_DIR_NONE      LEXANALYSIS_BASE +9
 #define LEXANALYSIS_DEPOT_DIR_SUBDIR    LEXANALYSIS_BASE +10
 #define LEXANALYSIS_DEPOT_DIR_BADSTRING LEXANALYSIS_BASE +11
-#define LEXANALYSIS_BASE_MAX				    LEXANALYSIS_BASE +20
+#define LEXANALYSIS_BASE_MAX			LEXANALYSIS_BASE +20
 
 // analysis levels
 #define LEXANALYSIS_LEVEL_0					0		// lex only
@@ -227,22 +223,22 @@ enum IteratorCode
 #define LEXANALYSIS_LEVEL_4					4		// lex + parse + syntax + detail + debug
 #define MIN_LEXANALYSIS_LEVEL				LEXANALYSIS_LEVEL_0
 #define MAX_LEXANALYSIS_LEVEL				LEXANALYSIS_LEVEL_4
-#define DEFAULT_LEXANALYSIS_LEVEL		LEXANALYSIS_LEVEL_2
+#define DEFAULT_LEXANALYSIS_LEVEL			LEXANALYSIS_LEVEL_2
 
 // syntax errors 
-#define RET_SYNTAX_BASE							RET_PROG_BASE + 30
-#define RET_SYNTAX_ERR							RET_SYNTAX_BASE + 0		// general or unknown syntax error
-#define RET_SYNTAX_WARN							RET_SYNTAX_BASE + 1		// general or unknown syntax warning
-#define RET_FILESYSTEM_SUBDIR_ERR		RET_SYNTAX_BASE + 3		// the directory entry has a sub directory
-#define RET_FILESYSTEM_STRING_ERR	  RET_SYNTAX_BASE + 4		// the entry has a bad string
-#define RET_DUPLICATE_ERR					  RET_SYNTAX_BASE + 5		// the entry appears more than once
+#define RET_SYNTAX_BASE						RET_PROG_BASE + 30
+#define RET_SYNTAX_ERR						RET_SYNTAX_BASE + 0		// general or unknown syntax error
+#define RET_SYNTAX_WARN						RET_SYNTAX_BASE + 1		// general or unknown syntax warning
+#define RET_FILESYSTEM_SUBDIR_ERR			RET_SYNTAX_BASE + 3		// the directory entry has a sub directory
+#define RET_FILESYSTEM_STRING_ERR			RET_SYNTAX_BASE + 4		// the entry has a bad string
+#define RET_DUPLICATE_ERR					RET_SYNTAX_BASE + 5		// the entry appears more than once
 #define RET_SYNTAX_BASE_MAX					RET_SYNTAX_BASE	+ 19
 
 // 'quiet' mode constants
-#define NOT_QUIET			3		// max output mode
-#define MAX_QUIET			0		// silent mode
-#define L_STATUS			3		// status message
-#define L_WARN				2		// warning
+#define NOT_QUIET				3		// max output mode
+#define MAX_QUIET				0		// silent mode
+#define L_STATUS				3		// status message
+#define L_WARN					2		// warning
 #define L_ERR					1		// error 
 #define L_REQ					0		// requested output
 #define QUIET_DEFAULT	L_ERR
@@ -278,8 +274,7 @@ enum IteratorCode
 #define DEFAULT_LEX_BUFSIZE 128
 #define	DEFAULT_ITEM_PREFIX             "FLD_"        // the header field element name prefix
 
-struct ANSIASCII_ANALYSIS_STATS
-{
+struct ANSIASCII_ANALYSIS_STATS {
 	int nSum;
 	int nErr;
 	int nUnknown;
